8505a5b1de94d4e7c794075bc12decfdd3f02c43
Steven Carathers <scarathers28@gmail.com>
2026-09-06 23:16:49 -0500

    modal touches

---
+++ index.js
@@ -0,0 +1,35 @@
+// template_uy4jfzl
+// service_c2ks6hv
+//yyn50K5lmN7c4U2lq
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_c2ks6hv',
+            'template_uy4jfzl',
+            event.target,
+            'yyn50K5lmN7c4U2lq'
+    ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+    }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at scarathers28@gmail.com"
+            );
+    })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
